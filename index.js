9fd6d9ada6281a016f63570a4cd6782588da8690
Mitchell <Hmitchell.hall@gmail.com>
2021-08-13 10:46:21 -0500

    commit tree 51cde2e78ed9b5731ad795c84e76883729a8a757

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function findWin(record){
+    return record.result === "W"
+}
+console.log(record.find(findWin))
+
+function superbowlWin(record){
+   return record.find(findWin) ? record.find(findWin).year : undefined}
+//record.find(findWin) ? record.find(findWin).year : undefined
